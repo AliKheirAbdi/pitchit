7c01057f1a67df3550f72bb8417bb312d07713f4
AliKheir <akheirali@gmail.com>
2019-10-20 20:36:23 +0300

    add static folder with main.css code

--- pitch.py
+++ pitch.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 posts = [
@@ -17,13 +18,16 @@ posts = [
 
 ]
 
+
 @app.route('/')
 def home():
     return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
