ed802c8ece798c80744f6a354ba4619e124ac4e0
AliKheir <akheirali@gmail.com>
2019-10-20 21:14:42 +0300

    add new user reg form route to pitch.py

--- forms.py
+++ forms.py
@@ -10,4 +10,14 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField( 'Remember Me')
+    submit = SubmitField('Login')
