26285a1f5de74e0c6865fd53773df3e26d29b5be
AliKheir <akheirali@gmail.com>
2019-10-21 17:41:53 +0300

    created validation check for already used usernames in forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from pitch.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -12,12 +13,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    def validate_username(self, username):
+        user  = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken, Please use a different one')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField( 'Remember Me')
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
