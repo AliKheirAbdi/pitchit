5ce12f73eaa56f032dc9dec24441f9c9b7387c01
AliKheir <akheirali@gmail.com>
2019-10-24 12:56:16 +0300

    deployment to heroku

--- app/models.py
+++ app/models.py
@@ -27,4 +27,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"cleare
+        return f"Post('{self.title}', '{self.date_posted}')"
