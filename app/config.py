56cffa2889fccb30f3b4b12a9eabc9a696a9ed6c
AliKheir <akheirali@gmail.com>
2019-10-24 13:11:38 +0300

    deployment to heroku

--- app/config.py
+++ app/config.py
@@ -1,8 +1,20 @@
-#  email configurations
 import os
 
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
