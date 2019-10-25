ec23c200dfbe8225daf8a1d8105f8b53a2ac75a9
AliKheir <akheirali@gmail.com>
2019-10-25 11:36:37 +0300

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,27 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from app.config import config_options
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-
-from app.users.routes import users
-from app.pitches.routes import pitches
-from app.main.routes import main
-app.register_blueprint(users)
-app.register_blueprint(pitches)
-app.register_blueprint(main)
-
 mail = Mail()
+
+
 
 def create_app(config_name):
     app = Flask(__name__)
-    #........
+    app.config.from_object(config_options[config_name])
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
     mail.init_app(app)
+
+    from app.users.routes import users
+    from app.pitches.routes import pitches
+    from app.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(pitches)
+    app.register_blueprint(main)
+
+    return app
+
+
